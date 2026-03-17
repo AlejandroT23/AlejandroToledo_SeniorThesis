8854c6e349c746ab072d1dc61c3090403b905d85
AlejandroT23 <atoledo1@carthage.edu>
2026-03-16 22:57:16 -0500

    Started Login Page

---
+++ design_flow/src/login.jsx
@@ -0,0 +1,37 @@
+'use strict'
+
+import { useState } from 'react'
+import { supabase } from './supabaseClient'
+
+function LoginPage() {
+
+    const [error, setError] = useState(null);
+
+    const handleGoogleLogin = async () => {
+        const {error} = await supabase.auth.signInWithOAuth(
+            {
+                provider: 'google'
+            }
+        )
+
+        if (error) {
+            setError("Sorry, please try again")
+        }
+    }
+
+    return (
+        <div>
+            <p>Temporary Page</p>
+            <h1>
+                Login Page
+            </h1>
+            <button onClick={handleGoogleLogin}>
+                Sign in with Google
+            </button>
+
+            {error && <p style={{color: 'red'}}>{error}</p>}
+        </div>
+    )
+}
+
+export default LoginPage
