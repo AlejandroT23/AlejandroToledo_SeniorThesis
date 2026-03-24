9bb1104bd10e2bc2e69075d8023717829700a9e2
AlejandroT23 <atoledo1@carthage.edu>
2026-03-24 17:17:39 -0500

    Database.js
    After practicing with supabase queries, I created a Database Abstraction file to store all queries that would be accessed in the program. Right now we are focused on the user creation

--- database.js
+++ database.js
@@ -1,1 +1,36 @@
-// Database queries go here // 
+import { supabase } from './supabaseClient'
+
+export async function getUser(userID) {
+    const {data, error} = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', userID)
+        .single();
+    if (error) {
+        return {data: null, error};
+    } else {
+        return {data, error: null};
+    }
+}
+
+export async function userExist(userID) {
+    const {data, error} = await supabase
+        .from('users')
+        .select('id')
+        .eq('id', userID)
+        .single();
+    if (error) {
+        return false;
+    } else {
+        return data;
+    }
+}
+
+export async function createUser({ id, first_name, last_name, avatar, google_drive_token}) {
+    const {data, error} = await supabase
+        .from('user')
+        .insert({id, first_name, last_name, avatar, google_driver_token})
+        .select()
+        .single();
+    return {data, error};
+}
