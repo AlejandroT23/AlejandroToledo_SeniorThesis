c3989e799d2c1a2ef9cf7679aa6d1d73c9e26b82
AlejandroT23 <atoledo1@carthage.edu>
2026-03-24 23:38:01 -0500

    Comment to reminder

--- design_flow/src/testpage.jsx
+++ design_flow/src/testpage.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Add logout button
+
 function Test() {
-    return (<div>Logged In</div>);
+    return (
+        <div>Logged In</div>
+    );
 }
 
 export default Test;
