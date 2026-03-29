091ad21f945de7f1e007b94115220b4f357fe450
AlejandroT23 <atoledo1@carthage.edu>
2026-03-28 23:52:43 -0500

    Update TeamComponent.jsx

--- design_flow/src/TeamComponent.jsx
+++ design_flow/src/TeamComponent.jsx
@@ -4,13 +4,13 @@ function TeamComponent({teams}) {
     return(
         <div>
             {teams.map((team, index) => (
-                <div key={team.team.id}>
-                    <p>{team.teams.team_name}</p>
-                    <p>{team.teams.deadline}</p>
-                    <p>{team.teams.desc}</p>
-                    <p>{team.teams.admin}</p>
-                    <p>{team.teams.icon}</p>
-                    <p>{team.teams.color}</p>
+                <div key={index}>
+                    <p>{team.teams?.team_name}</p>
+                    <p>{team.teams?.deadline}</p>
+                    <p>{team.teams?.desc}</p>
+                    <p>{team.teams?.admin}</p>
+                    <p>{team.teams?.icon}</p>
+                    <p>{team.teams?.color}</p>
                 </div>
             ))}
         </div>
