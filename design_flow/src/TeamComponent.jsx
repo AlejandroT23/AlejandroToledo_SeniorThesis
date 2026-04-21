578227b33b1fa82c271e880a3a94d4405ee297f4
AlejandroT23 <atoledo1@carthage.edu>
2026-04-21 09:21:31 -0500

    Styling for team component

--- design_flow/src/TeamComponent.jsx
+++ design_flow/src/TeamComponent.jsx
@@ -1,18 +1,27 @@
-// 
+import './styles/teamComponent.css' 
 
 function TeamComponent({teams, onTeamClick}) {
     return(
-        <div>
-            {teams.map((team, index) => (
-                <div key={index} onClick={()=>onTeamClick(team.id)}>
-                    <p>{team.team_name}</p>
-                    <p>{team.deadline}</p>
-                    <p>{team.desc}</p>
-                    <p>{team.admin}</p>
-                    <p>{team.icon}</p>
-                    <p>{team.color}</p>
-                </div>
-            ))}
+        <div className="team_list">
+            <h2 className="team_list_header">Teams</h2>
+            <div class="team_list_body">
+                {teams.map((team, index) => (
+                    <div key={index} className="team_card" onClick={()=>onTeamClick(team.id)}>
+                        <div className="team_card_avatar"></div>
+                        <div className="team_card_info">
+                            <p className="team_card_name"> {team.team_name} </p>
+                            <p className="team_card_desc">  {team.desc} </p>
+                        </div>
+                        <div className = "team_card_menu">:</div>
+                        {/* <p>{team.team_name}</p>
+                        <p>{team.deadline}</p>
+                        <p>{team.desc}</p>
+                        <p>{team.admin}</p>
+                        <p>{team.icon}</p>
+                        <p>{team.color}</p> */}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
